9e09557db7d71879e0436472d8e875455641df5c
JoeriSmits <joeri@webbio.nl>
2024-11-18 14:57:04 +0100

    feat: refactored substr to split for support outside eu-west-1 region

--- modules/csnotification/csnotification.tf
+++ modules/csnotification/csnotification.tf
@@ -8,13 +8,12 @@ resource "aws_codestarnotifications_notification_rule" "notification-rule" {
   for_each = toset(var.codenotification_pipeline_arn)
   detail_type = "FULL"
   event_type_ids = ["codepipeline-pipeline-pipeline-execution-started","codepipeline-pipeline-pipeline-execution-succeeded", "codepipeline-pipeline-pipeline-execution-failed","codepipeline-pipeline-manual-approval-needed", "codepipeline-pipeline-manual-approval-succeeded"]
-  name = "rule-${substr(each.key,44,-1)}"
+  name = "rule-${split(each.key, ":", 5)}"
 
   resource = each.key
 
   target {
     address = module.slack-chatbot.notification-bot-configuration-arn
     type = "AWSChatbotSlack"
-
   }
 }
