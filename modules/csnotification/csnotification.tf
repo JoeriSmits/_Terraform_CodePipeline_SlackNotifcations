0c73e7c3f2af0afcaae1e3dc0185aa7b81dbc4da
JoeriSmits <joeri@webbio.nl>
2024-11-18 14:58:03 +0100

    feat: refactored substr to split for support outside eu-west-1 region

--- modules/csnotification/csnotification.tf
+++ modules/csnotification/csnotification.tf
@@ -8,7 +8,7 @@ resource "aws_codestarnotifications_notification_rule" "notification-rule" {
   for_each = toset(var.codenotification_pipeline_arn)
   detail_type = "FULL"
   event_type_ids = ["codepipeline-pipeline-pipeline-execution-started","codepipeline-pipeline-pipeline-execution-succeeded", "codepipeline-pipeline-pipeline-execution-failed","codepipeline-pipeline-manual-approval-needed", "codepipeline-pipeline-manual-approval-succeeded"]
-  name = "rule-${split(each.key, ":", 5)}"
+  name = "rule-${split(each.key, ":")[5]}"
 
   resource = each.key
 
