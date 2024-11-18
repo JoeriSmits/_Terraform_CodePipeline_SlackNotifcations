c6a10ba59f0dbeda50dd74dab9a4068f32546fde
JoeriSmits <joeri@webbio.nl>
2024-11-18 15:07:07 +0100

    fix: split function for name and last element

--- modules/csnotification/csnotification.tf
+++ modules/csnotification/csnotification.tf
@@ -4,14 +4,17 @@ module "slack-chatbot" {
   slack_channel_id = var.slack_channel_id
 }
 
+locals {
+  code_pipeline_arn_split = 
+}
+
 resource "aws_codestarnotifications_notification_rule" "notification-rule" {
   for_each = toset(var.codenotification_pipeline_arn)
   detail_type = "FULL"
   event_type_ids = ["codepipeline-pipeline-pipeline-execution-started","codepipeline-pipeline-pipeline-execution-succeeded", "codepipeline-pipeline-pipeline-execution-failed","codepipeline-pipeline-manual-approval-needed", "codepipeline-pipeline-manual-approval-succeeded"]
-  name = "rule-${element(split(each.key, ":"), 5)}"
+  name = "rule-${element(split(":", each.key), length(split(":", each.key)) - 1)}"
 
   resource = each.key
-
   target {
     address = module.slack-chatbot.notification-bot-configuration-arn
     type = "AWSChatbotSlack"
